e94b2492dfeea162b8e132cbce832cadf4dfa540
pushyami <pushyami@umich.edu>
2020-01-15 16:36:44 -0500

    omething:

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ load_dotenv(dotenv_path=os.path.dirname(os.path.abspath(__file__)) + "/.env")
 def main():
     placement_exam = os.getenv('placement_exam')
     path_properties = os.getenv('path_properties')
+    print(f"Path to properties {path_properties}")
+    print(f"Placement exam: {placement_exam}")
     with open(path_properties, 'r') as yml_file:
         sf = yaml.load(yml_file, Loader=yaml.FullLoader)
     print(sf[placement_exam]['id'])
