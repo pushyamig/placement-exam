1fb87e2648fdfe2e39a39dea67fe119645514fd8
pushyami <pushyami@umich.edu>
2020-01-15 16:27:14 -0500

    adding props path

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ load_dotenv(dotenv_path=os.path.dirname(os.path.abspath(__file__)) + "/.env")
 
 def main():
     placement_exam = os.getenv('placement_exam')
-    with open('properties.yaml', 'r') as yml_file:
+    path_properties = os.getenv('path_properties')
+    with open(path_properties, 'r') as yml_file:
         sf = yaml.load(yml_file, Loader=yaml.FullLoader)
     print(sf[placement_exam]['id'])
 
