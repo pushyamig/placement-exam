2a68639741175586fba1ec86b567ea5c4aff7b08
pushyami <pushyami@umich.edu>
2020-01-17 12:26:42 -0500

    Adding API directory support

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,12 @@ WORKDIR /app/
 
 COPY . /app/
 
+ENV API_UTILS_VERSION v1.3
+RUN pip install git+https://github.com/tl-its-umich-edu/api-utils-python@${API_UTILS_VERSION}
 RUN pip install -r requirements.txt
+
+# Sets the local timezone of the docker image
+ENV TZ=America/Detroit
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+
+import pkg_resources
 import yaml
 from dotenv import load_dotenv
+from umich_api.api_utils import ApiUtil
 
 load_dotenv(dotenv_path=os.path.dirname(os.path.abspath(__file__)) + "/.env")
 
@@ -12,7 +15,16 @@ def main():
     print(f"Placement exam: {placement_exam}")
     with open(path_properties, 'r') as yml_file:
         sf = yaml.load(yml_file, Loader=yaml.FullLoader)
-    print(sf[placement_exam]['id'])
+    print(sf['course'][placement_exam]['id'])
+    client_ = sf['api']['client']
+    secret_ = sf['api']['secret']
+    url_ = sf['api']['url']
+    print(client_)
+    print(secret_)
+    print(url_)
+    api_json: str = pkg_resources.resource_filename(__name__, 'apis.json')
+    api_handler = ApiUtil(url_, client_, secret_, api_json)
+    print(api_handler.get_access_token('aa/oauth2/token','spanishplacementscores'))
 
 
 if __name__ == '__main__':
